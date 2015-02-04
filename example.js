83f6ea6a0c922e479e9da82e52cac5e52f326434
apelsyn <oleg.cherniy@gmail.com>
2015-02-04 12:34:07 +0200

    Cosmetic fixes

--- example.js
+++ example.js
@@ -6,15 +6,12 @@ var testMethodPromise = function testGenerator (a) {
 };
 
 cacher(testMethodPromise,[4],{ cacheTime: 3 }).done(function (data) {
-        console.log('first launch: ' + data); // wait 1 sec and get '5'
-        cacher(testMethodPromise,[4],3).done(function (data) { // { cacheTime: 3 } equivalent '3'
-            console.log('second launch: ' + data); //  get '5' from cache immediately
-        },
-        function (err) {
-            throw err;
-        });
-    },
-    function (err) {
-        throw err;
-    }
-);
+    console.log('first launch: ' + data); // wait 1 sec and get '5'
+    cacher(testMethodPromise,[4],3).done(function (data) { // { cacheTime: 3 } equivalent '3'
+        console.log('second launch: ' + data); //  get '5' from cache immediately
+    },onerror);
+},onerror);
+
+function onerror(err) {
+    console.error(err.stack);
+}
