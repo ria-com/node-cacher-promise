9c1fd1fae23c8bc94660de2ceaced0189b88261d
master <oleg.cherniy@gmail.com>
2016-07-27 10:38:33 +0300

    Fix: change q method 'done' to standart 'then'

--- example.js
+++ example.js
@@ -5,9 +5,9 @@ var testMethodPromise = function testMethodPromise (a) {
     return Q.delay(a+1, 1000);
 };
 
-cacher(testMethodPromise,[4],{ expires: 3 }).done(function (data) {
+cacher(testMethodPromise,[4],{ expires: 3 }).then(function (data) {
     console.log('first launch: ' + data); // wait 1 sec and get '5'
-    cacher(testMethodPromise,[4],3).done(function (data) { // { expires: 3 } equivalent '3'
+    cacher(testMethodPromise,[4],3).then(function (data) { // { expires: 3 } equivalent '3'
         console.log('second launch: ' + data); //  get '5' from cache immediately
     },onerror);
 },onerror);
